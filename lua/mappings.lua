30e74cc079a2880d2d8ab75e2c71bc2d435ef5cc
Reo Tamai <reotamai5@gmail.com>
2024-10-15 08:29:24 -0700

    silent move line

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
@@ -14,7 +14,6 @@ map("n", "N", "Nzzzv")
 map("x", "<leader>p", '"_dP')
 map("n", "<leader>d", '"_d')
 map("v", "<leader>d", '"_d')
-
 
 -- Move insert mode
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
@@ -40,11 +39,9 @@ end, { desc = "General Format file" })
 
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
 
-
 -- Comment
 map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
 map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
-
 
 -- telescope
 map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
@@ -53,8 +50,12 @@ map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = 
 map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
 map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
-map("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", { desc = "telescope find all files" })
-
+map(
+    "n",
+    "<leader>fa",
+    "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+    { desc = "telescope find all files" }
+)
 
 -- whichkey
 map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
@@ -63,40 +64,33 @@ map("n", "<leader>wk", function()
     vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
 end, { desc = "whichkey query lookup" })
 
-
--- oil 
+-- oil
 vim.keymap.set("n", "<leader>e", "<cmd>Oil --float <CR>", { desc = "open oil" })
 
-
 -- leap
-map({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-map({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
-
+map({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+map({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
 
 -- undotree
-map('n', '<leader>u', '<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>')
-
+map("n", "<leader>u", "<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>")
 
 -- tabufline
 map("n", "<tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "buffer goto next" })
 map("n", "<S-tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "buffer goto prev" })
 map("n", "<leader>q", "<cmd>bd!<cr>", { desc = "buffer close" })
 
-
 -- diffview
-map("n", "<leader>di", function ()
+map("n", "<leader>di", function()
     if require("diffview.lib").get_current_view() == nil then
-        vim.cmd("DiffviewOpen")
+        vim.cmd "DiffviewOpen"
     else
-        vim.cmd("DiffviewClose")
+        vim.cmd "DiffviewClose"
     end
 end, { desc = "toggle diffview" })
-
 
 -- toggleterm
 map("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>")
 map("t", "<C-q>", "<cmd>ToggleTermToggleAll<CR>")
-
 
 -- lsp saga
 map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "code action" })
@@ -105,4 +99,3 @@ map("n", "gD", "<cmd>Lspsaga goto_definition<CR>", { desc = "go to definition" }
 map("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { desc = "rename" })
 map("n", "<leader>dn", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "diagnostic next" })
 map("n", "<leader>dp", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "diagnostic previous" })
-
