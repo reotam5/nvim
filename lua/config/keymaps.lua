d1c79ba33e70961b0fb40cb373ba1a3300d0b97d
Reo Tamai <reotamai5@gmail.com>
2025-06-27 15:18:33 -0700

    lazyvim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- jk in insert mode to be escape
+vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to escape" })
+
+-- move selected up/down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected up" })
+
+-- ctrl + d to discard file changes
+vim.keymap.set("n", "<C-d>", ":e!<CR>", { desc = "Discard file changes" })
