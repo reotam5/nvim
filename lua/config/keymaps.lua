cdca58ef84b29a0ec521d24e05745ea95c5772aa
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2025-06-30 14:12:05 -0700

    vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,18 +2,29 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- jk in insert mode to be escape
-vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to escape" })
+if vim.g.vscode then
+  -- vscode specific keymaps
+  local vscode = require("vscode")
 
+  -- ctrl s to save file
+  vim.keymap.set("n", "<C-s>", function()
+    vscode.action("workbench.action.files.save")
+  end, { desc = "Save file" })
+else
+  -- jk in insert mode to be escape
+  vim.keymap.set("i", "jk", "<Esc>", { desc = "jk to escape" })
+
+  -- ctrl + d to discard file changes
+  vim.keymap.set("n", "<C-d>", ":e!<CR>", { desc = "Discard file changes" })
+
+  -- open terminal
+  vim.keymap.set("n", "<leader>t", ":terminal<CR>", { desc = "Open terminal" })
+
+  -- exit terminal mode
+  vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+end
+
+-- shared keymaps
 -- move selected up/down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected up" })
-
--- ctrl + d to discard file changes
-vim.keymap.set("n", "<C-d>", ":e!<CR>", { desc = "Discard file changes" })
-
--- open terminal
-vim.keymap.set("n", "<leader>t", ":terminal<CR>", { desc = "Open terminal" })
-
--- exit terminal mode
-vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
