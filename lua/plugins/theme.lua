a704dc80544b3b17fd8ee1749fc07d30400fb630
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2025-10-09 20:05:42 -0700

    theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,13 @@
 return {
-  "EdenEast/nightfox.nvim",
+  "neanias/everforest-nvim",
   version = false,
   lazy = false,
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("nightfox").setup({})
-    vim.cmd([[colorscheme dayfox]])
+    require("everforest").setup({
+      background = "medium", -- hard, medium, soft
+    })
+    vim.o.background = "dark" -- light, dark
+    vim.cmd([[colorscheme everforest]])
   end,
 }
