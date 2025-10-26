f214307ae03c6c596faa8f800a8135e20d310e39
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2025-10-25 21:39:32 -0700

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,10 @@
 return {
-  "neanias/everforest-nvim",
+  "ellisonleao/gruvbox.nvim",
   version = false,
   lazy = false,
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("everforest").setup({
-      background = "medium", -- hard, medium, soft
-    })
-    vim.o.background = "dark" -- light, dark
-    vim.cmd([[colorscheme everforest]])
+    vim.o.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
   end,
 }
